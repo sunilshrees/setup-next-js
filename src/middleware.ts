a18c8c79294137fc71dfa752200d735fb2f4d79f
sunilshrees <sunil.everestwalk@gmail.com>
2025-06-28 21:25:27 +0545

    initial setup

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   // Just return the same URL as a redirect
-  return NextResponse.redirect(new URL(request.nextUrl.pathname, request.url));
+  // return NextResponse.redirect(new URL(request.nextUrl.pathname, request.url));
 }
 
 export const config = {
